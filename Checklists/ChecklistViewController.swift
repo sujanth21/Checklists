7f4d7ec97c93b4c450216a5ecf4bba39652af184
Sujanth <sujanth1987@gmail.com>
2017-07-24 08:11:12 +1000

    Created array objects for Checklist Items

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -10,32 +10,35 @@ import UIKit
 
 class ChecklistViewController: UITableViewController {
     
-    var row0Item: ChecklistItem
-    var row1Item: ChecklistItem
-    var row2Item: ChecklistItem
-    var row3Item: ChecklistItem
-    var row4Item: ChecklistItem
+    var items: [ChecklistItem]
     
     required init?(coder aDecoder: NSCoder) {
-        row0Item = ChecklistItem()
+        items = [ChecklistItem]()
+        
+        let row0Item = ChecklistItem()
         row0Item.text = "Walk the dog"
         row0Item.checked = false
+        items.append(row0Item)
         
-        row1Item = ChecklistItem()
+        let row1Item = ChecklistItem()
         row1Item.text = "Brush my teeth"
         row1Item.checked = false
+        items.append(row1Item)
         
-        row2Item = ChecklistItem()
+        let row2Item = ChecklistItem()
         row2Item.text = "Learn iOS development"
         row2Item.checked = false
+        items.append(row2Item)
         
-        row3Item = ChecklistItem()
+        let row3Item = ChecklistItem()
         row3Item.text = "Cricket practice"
         row3Item.checked = false
+        items.append(row3Item)
         
-        row4Item = ChecklistItem()
+        let row4Item = ChecklistItem()
         row4Item.text = "Eat Ice Cream"
         row4Item.checked = false
+        items.append(row4Item)
         
         super.init(coder: aDecoder)
     }
