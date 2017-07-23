6f38922776d073422a06125f27190e3fa0889228
Sujanth <sujanth1987@gmail.com>
2017-07-23 13:49:38 +1000

    Added sample data to test

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -22,11 +22,26 @@ class ChecklistViewController: UITableViewController {
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistIdentifier", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row == 3 {
+            label.text = "Cricket practice"
+        } else if indexPath.row == 4 {
+            label.text = "Eat Ice Cream"
+        }
+        
         return cell
     }
 }
