f28f452893b4dde4b428367c4a1489edaa35c32e
Sujanth <sujanth1987@gmail.com>
2017-07-23 18:33:59 +1000

    Added tableview didSelectRowAt method

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -22,7 +22,7 @@ class ChecklistViewController: UITableViewController {
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -30,19 +30,32 @@ class ChecklistViewController: UITableViewController {
         
         let label = cell.viewWithTag(1000) as! UILabel
         
-        if indexPath.row == 0 {
+        if indexPath.row % 5 == 0 {
             label.text = "Walk the dog"
-        } else if indexPath.row == 1 {
+        } else if indexPath.row % 5 == 1 {
             label.text = "Brush my teeth"
-        } else if indexPath.row == 2 {
+        } else if indexPath.row % 5 == 2 {
             label.text = "Learn iOS development"
-        } else if indexPath.row == 3 {
+        } else if indexPath.row % 5 == 3 {
             label.text = "Cricket practice"
-        } else if indexPath.row == 4 {
+        } else if indexPath.row % 5 == 4 {
             label.text = "Eat Ice Cream"
         }
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
