71960c999a90459acb917712a4ec03360922a828
Sujanth <sujanth1987@gmail.com>
2017-07-24 20:39:14 +1000

    Added cancel and done buttons in the add item screen

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Sujanth Sebamalaithasan on 24/7/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddItemViewController: UITableViewController {
+    
+    @IBAction func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func Done() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
