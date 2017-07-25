dfbe0de54d8e244abd3de504029d1179efab6879
Sujanth <sujanth1987@gmail.com>
2017-07-26 08:01:55 +1000

    Passing data to Edit item screen

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -147,6 +147,17 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
             let controller = navigationController.topViewController as! AddItemViewController
             
             controller.delegate = self
+        } else if segue.identifier == "EditItem" {
+            
+            let navigationController = segue.destination as! UINavigationController
+            
+            let controller = navigationController.topViewController as! AddItemViewController
+            
+            controller.delegate = self
+            
+            if let indexPath = tableView.indexPath(for: sender as! UITableViewCell) {
+                controller.itemToEdit = items[indexPath.row]
+            }
         }
     }
     
