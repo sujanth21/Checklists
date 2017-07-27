7bfa80a2bfd655c419e276bcdb3bc4180ed0c7dc
Sujanth <sujanth1987@gmail.com>
2017-07-27 22:52:29 +1000

    Add/Edit checklist created

--- Checklists/ListDetailViewControllerTableViewController.swift
+++ Checklists/ListDetailViewControllerTableViewController.swift
@@ -56,6 +56,35 @@ class ListDetailViewController: UITableViewController, UITextFieldDelegate {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        
+        doneBarButton.isEnabled = (newText.length > 0)
+        return true
+    }
+    
+    
+    @IBAction func cancel() {
+        delegate?.listDetailViewControllerDidCancel(self)
+    }
+    
+    @IBAction func done() {
+        
+        if let checklist = checklistToEdit {
+            checklist.name = textField.text!
+            delegate?.listDetailViewController(self, didFinishEditing: checklist)
+        } else {
+            let checklist = Checklist(name: textField.text!)
+            delegate?.listDetailViewController(self, didFinishAdding: checklist)
+        }
+    }
 
    
 
