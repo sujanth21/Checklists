77d7328cf82aeb0d25bd45bfe5d43dc69d62ad33
Sujanth <sujanth1987@gmail.com>
2017-07-27 21:47:39 +1000

    Added prepare for segue method in AllListViewController

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -61,10 +61,16 @@ class AllListsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "ShowChecklist", sender: nil)
+        let checklist = lists[indexPath.row]
+        performSegue(withIdentifier: "ShowChecklist", sender: checklist)
     }
 
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowChecklist" {
+            let controller = segue.destination as! ChecklistViewController
+            controller.checklist = sender as! Checklist
+        }
+    }
     
     func makeCell(for tableView: UITableView) -> UITableViewCell {
         
--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ChecklistViewController: UITableViewController, ItemDetailViewControllerDelegate {
     
     var items: [ChecklistItem]
+    var checklist: Checklist!
     
     required init?(coder aDecoder: NSCoder) {
         items = [ChecklistItem]()
@@ -20,7 +21,8 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = checklist.name
     }
 
     override func didReceiveMemoryWarning() {
