b950d142485439e06f3a4eb8d66472b7b763a6e0
Sujanth <sujanth1987@gmail.com>
2017-07-28 23:47:58 +1000

    First time list added

--- Checklists/DataModel.swift
+++ Checklists/DataModel.swift
@@ -55,12 +55,29 @@ class DataModel {
     init() {
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
     
     func registerDefaults() {
         
-        let dictionary: [String: Any] = ["ChecklistIndex": -1]
+        let dictionary: [String: Any] = ["ChecklistIndex": -1, "FirstTime": true]
         UserDefaults.standard.register(defaults: dictionary)
+    }
+    
+    func handleFirstTime() {
+        
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime {
+            
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+            userDefaults.synchronize()
+        }
     }
     
 }
