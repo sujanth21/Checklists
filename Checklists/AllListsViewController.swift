9935d93b15e976a948d8986d7885c6ef02ab3e33
Sujanth <sujanth1987@gmail.com>
2017-07-29 20:27:12 +1000

    checklist icons added

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -65,7 +65,7 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
             cell.detailTextLabel!.text = "\(count) Remaining"
         }
         
-        
+        cell.imageView!.image = UIImage(named: checklist.iconName)
         
         return cell
     }
--- Checklists/Checklist.swift
+++ Checklists/Checklist.swift
@@ -12,21 +12,25 @@ class Checklist: NSObject, NSCoding {
 
     var name = ""
     var items = [ChecklistItem]()
+    var iconName: String
     
     init(name: String) {
         self.name = name
+        iconName = "Appointments"
         super.init()
     }
     
     required init?(coder aDecoder: NSCoder) {
         name = aDecoder.decodeObject(forKey: "Name") as! String
         items = aDecoder.decodeObject(forKey: "Items") as! [ChecklistItem]
+        iconName = aDecoder.decodeObject(forKey: "IconName") as! String
         super.init()
     }
     
     func encode(with aCoder: NSCoder) {
         aCoder.encode(name, forKey: "Name")
         aCoder.encode(items, forKey: "Items")
+        aCoder.encode(iconName, forKey: "IconName")
     }
     
     func countUncheckedItems() -> Int {
