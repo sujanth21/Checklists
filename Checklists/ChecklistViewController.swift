ecd52903f1427b0ea612d4dec7907d224e38e58c
Sujanth <sujanth1987@gmail.com>
2017-07-29 21:49:16 +1000

    Added icons and launch screen

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -65,6 +65,7 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
     func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
       
         let label = cell.viewWithTag(1001) as! UILabel
+        label.textColor = view.tintColor
         
         if item.checked {
             label.text = "√"
